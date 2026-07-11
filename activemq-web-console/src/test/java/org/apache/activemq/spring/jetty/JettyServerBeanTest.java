180749caf801b9a2070c43e9fffac930691c9373
Matt Pavlovich <mattrpav@apache.org>
2026-07-11 13:23:23 -0500

    [#] Add tests for security wiring to align with 6.2.x hardening

--- activemq-web-console/src/test/java/org/apache/activemq/spring/jetty/JettyServerBeanTest.java
+++ activemq-web-console/src/test/java/org/apache/activemq/spring/jetty/JettyServerBeanTest.java
@@ -1,7 +1,17 @@
 package org.apache.activemq.spring.jetty;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.junit.Test;
 
 public class JettyServerBeanTest {
@@ -11,8 +21,24 @@ public class JettyServerBeanTest {
         var context = new ClassPathXmlApplicationContext("conf/jetty-spring.xml");
         context.start();
 
-        var jettyServer = (Server) context.getBean("jettyServer");
-        // assertNotNull(jettyServer);
+        var jettyServer = (JettyServerBean) context.getBean("jettyServer");
+        assertNotNull(jettyServer);
+
+        Server server = jettyServer.getServer();
+        assertNotNull("Embedded Jetty Server should be created", server);
+        assertTrue("Embedded Jetty Server should be running", server.isRunning());
+
+        // The console is protected by the JAAS SecurityHandler wired in
+        // jetty-security.xml: an unauthenticated request must be challenged
+        // (HTTP 401) rather than served. A request from loopback also confirms
+        // the InetAccessHandler allows local clients.
+        int port = ((ServerConnector) server.getConnectors()[0]).getLocalPort();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(
+                HttpRequest.newBuilder(URI.create("http://127.0.0.1:" + port + "/admin/")).GET().build(),
+                HttpResponse.BodyHandlers.ofString());
+        assertEquals("Web console must require authentication", 401, response.statusCode());
+
         context.stop();
     }
 }
